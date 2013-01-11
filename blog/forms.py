67e996d1c7dad014f6cb861f95795a4f7d6872f1
Dammy Kolawole <dkolawole@idirect.net>
2013-01-11 16:54:52 +0000

    rearranged fields, add field attributes

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,14 @@
 # from django.forms import ModelForm
-from django import forms
+from django.forms import ModelForm, Textarea, TextInput
 from blog.models import Comment
 
-class CommentForm(forms.ModelForm):
+class CommentForm(ModelForm):
 	# author = forms.CharField(label='',)
 	
 	class Meta:
 		model = Comment
-		fields = ('author', 'body')
+		fields = ('body','author')
+		widgets = {
+			'author': TextInput({'placeholder': 'Name', 'class':'input-medium span2'}),
+            'body': Textarea(attrs={'rows': 4, 'placeholder': 'Add a comment . . .', 'class': 'custom'})
+        }

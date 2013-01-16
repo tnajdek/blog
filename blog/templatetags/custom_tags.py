5a34f915b6d18db8d02d0abe6dbbece7b0c2def6
Dammy Kolawle <dammy@unknown-e0:f8:47:06:5d:e2.home>
2013-01-16 08:43:21 +0000

    rever to IF-ELSE due to bug using the loop version; loop version is bug-free when run as a standalone script

--- blog/templatetags/custom_tags.py
+++ blog/templatetags/custom_tags.py
@@ -21,16 +21,17 @@ def trim(value):
 
 @register.filter
 def month_name(month_number):
-    return calendar.month_name[month_number]
+	return calendar.month_name[month_number]
 
 def pluralize(value, string):
 	if value > 1:
 		return string + 's'
-	return string
+	else:
+		return string
 
 # python version < 2.7 don't have deltatime.total_seconds() so I'm using this
 def total_seconds(td):
-    return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+	return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
 
 @register.filter
 def prettify_date(timestamp):
@@ -41,24 +42,24 @@ def prettify_date(timestamp):
 	post_timestamp = timestamp.replace(tzinfo=None) # strip off timezone before subtracting else you get this "can't subtract offset-naive and offset-aware datetimes"
 	timestamp_difference = current_timestamp - post_timestamp
 
-	timestamp_difference_in_seconds = total_seconds(timestamp_difference)
-	for index, item in enumerate(time_unit_list):
-
-		for time_unit in item:
-			unit_operand = item[time_unit]
-
-			if timestamp_difference_in_seconds < unit_operand:
-				ret = 'a moment ago'
-
-			if index > 0:
-				if timestamp_difference_in_seconds < unit_operand:
-
-					time_unit_dict = time_unit_list[index - 1]
-					time_unit_key = time_unit_dict.keys()[0]
-					time_unit_value = time_unit_dict[time_unit_key]
-					
-					pretty_value = int(round(timestamp_difference_in_seconds / time_unit_value))
-					ret = 'about {0} {1} ago'.format(pretty_value, pluralize(pretty_value, time_unit_key))
-				else:
-					ret = 'over a year ago'
-	return ret
+	timestamp_difference_in_seconds = timestamp_difference.total_seconds() #total_seconds(timestamp_difference)
+	
+	if timestamp_difference_in_seconds < MINUTE:
+		return 'just now'
+	elif timestamp_difference_in_seconds < HOUR:
+		pretty_value = int(round(timestamp_difference_in_seconds / MINUTE))
+		return 'about {0} {1} ago'.format(pretty_value, pluralize(pretty_value, 'minute')) 
+	elif timestamp_difference_in_seconds < DAY:
+		pretty_value = int(round(timestamp_difference_in_seconds / HOUR))
+		return 'about {0} {1} ago'.format(pretty_value, pluralize(pretty_value, 'hour'))
+	elif timestamp_difference_in_seconds < WEEK:
+		pretty_value = int(round(timestamp_difference_in_seconds / DAY))
+		return 'about {0} {1} ago'.format(pretty_value, pluralize(pretty_value, 'day'))
+	elif timestamp_difference_in_seconds < MONTH:
+		pretty_value = int(round(timestamp_difference_in_seconds / WEEK))
+		return 'about {0} {1} ago'.format(pretty_value, pluralize(pretty_value, 'week'))
+	elif timestamp_difference_in_seconds < YEAR:
+		pretty_value = int(round(timestamp_difference_in_seconds / MONTH))
+		return 'about {0} {1} ago'.format(pretty_value, pluralize(pretty_value, 'month'))
+	else:
+		return 'over a year ago'

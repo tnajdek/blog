f9a668ab08952d5ef7c88e0b0b76ad8d8aa4efad
Tom Najdek <tom@doppnet.com>
2013-01-17 21:20:21 +0000

    Fixed hashbang. Added some comments.

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 import os, sys, json, subprocess, getopt, argparse
 
 
@@ -19,6 +19,10 @@ except ValueError, ve:
 	# raise
 else:
 	for django_package in data['dependencies']:
+		# This won't work on many systems: majority of folks will have 2 pips by now
+		# One for python 2.7.x and one for python 3.x hence naming them pip2 and pip3 or simarly
+		# on my Arch pip is actually linked to pip3 which causes all crazy stuff
+		# hence requirements.txt is the way forward :)
 		command = ['pip', 'install', django_package]
 
 		p = subprocess.Popen(command, stdout=subprocess.PIPE)
